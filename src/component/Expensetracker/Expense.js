e12ea9aaf04fc774782addfe043670e25a80ca52
vibinab <abvibin4@gmail.com>
2022-08-01 19:40:40 +0530

    email

--- src/component/Expensetracker/Expense.js
+++ src/component/Expensetracker/Expense.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import React ,{useContext, useState} from 'react';
 import "./Expense.css";
 import { NavLink } from 'react-router-dom';
+import axios from "axios";
+import AuthContext from '../../store/auth-context';
+
 
 
 export const Expense = () => {
+
+    const [verifyemail, setverifyemail]=useState('');
+    const authctx=useContext(AuthContext)
+
+    const emailhandler=(event)=> {
+        setverifyemail(event.target.value)
+    }
+
+    const submitemailhandler=(event)=> {
+        event.preventDefault();
+
+        const emailtoverify=verifyemail;
+        console.log(emailtoverify)
+
+       fetch('https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyBIgjFkM6qpnh2_mVUR1jWlgfuJfjlCpyc', {
+        method:'POST',
+        body:JSON.stringify({
+            requestType: "VERIFY_EMAIL",
+            idToken:authctx.token
+
+
+        })
+
+       }).then((res)=> {
+        if(res.ok){
+            return res.json()
+        }
+        else {
+
+            res.json().then((data)=> {
+                let err;
+                if(data && data.error && data.error.message){
+                    err=data.error.message
+                }
+                throw new Error(err)
+            })
+
+        }
+        
+       })
+       .then((data)=> {
+        console.log(data)
+       })
+       .catch((err)=> {
+        console.log(err)
+       
+       })
+    
+
+    }
   return (
     <>
         <div className='expensehead'>
@@ -14,6 +67,15 @@ export const Expense = () => {
             <p>Your profile is incomplete <NavLink to="/profile">complete now</NavLink></p>
         </div>
 
+        </div>
+        <div className='verifyemail'>
+            <div>
+                <form onSubmit={submitemailhandler}>
+                <label>Verify email</label>
+                <input type="email" onChange={emailhandler}></input>
+                <button type="submit">verify</button>
+                </form>
+            </div>
         </div>
     </>
   )
--- src/component/profile/Profile.js
+++ src/component/profile/Profile.js
@@ -136,6 +136,7 @@ export const Profile = () => {
                  }
             </form>
         </div>
+        
     </>
   )
 }
