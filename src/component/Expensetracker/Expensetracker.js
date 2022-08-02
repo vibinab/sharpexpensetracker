0af14f6b657d45293345826209a9c94d292b6097
vibinab <abvibin4@gmail.com>
2022-08-02 15:39:12 +0530

    getandpost

--- src/component/Expensetracker/Expensetracker.js
+++ src/component/Expensetracker/Expensetracker.js
@@ -1,16 +1,20 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Expensetracker.css"
 import { Expensetrackeritem } from './Expensetrackeritem'
+import axios from "axios"
 
 export const Expensetracker = () => {
 
-    const [data,setdata]=useState([{}])
+    // const [data,setdata]=useState([{}])
+    const [dataapi,setdataapi]=useState([{}]);
 
     const [spend,setpend]=useState('');
 
     const[ desc,setdesc]=useState('');
 
     const[ category,setcategory]=useState('');
+
+    const [error, seterror]=useState(null);
 
     const spendhandler=(event)=> {
         setpend(event.target.value)
@@ -31,9 +35,59 @@ export const Expensetracker = () => {
             category:category
         }
         console.log(expensedata)
-        setdata( current=>[...current,expensedata])
-        console.log(data)
+        // setdata( current=>[...current,expensedata])
+        // console.log(data)
+        axios.post('https://expensetracker-ed764-default-rtdb.firebaseio.com/e.json', {
+            body:expensedata,
+          
+        }).then((res)=>{
+
+            console.log(res)
+            // setdataapi(res.data.name)
+            // console.log(dataapi)
+            // seterror(res.status)
+            
+        })
+        .catch((err)=> {
+            console.log(err)
+        })
+
+        setpend(" ")
+        setdesc(" ")
+        setcategory(" ")
     }
+
+    useEffect(()=> {
+        axios.get('https://expensetracker-ed764-default-rtdb.firebaseio.com/e.json')
+        .then((res)=>{
+            console.log("get",res.data)
+           let loaddata=[]
+            for (const key in res.data){
+                console.log("Data",res.data[key].body)
+                 loaddata.push(res.data[key].body)
+            }
+            console.log("show data",loaddata)
+           setdataapi(loaddata)
+           
+           
+            
+            // const loadedMovies=[];
+            // for(const key in  res.data){
+            //  loadedMovies.push({
+            //    id:key,
+            //    spend:res.data[key].spend,
+            //    desc:res.data[key].desc,
+            //    category:res.data[key].category,
+            //  });
+            // }
+            // console.log(loadedMovies)
+        })
+        .catch((err)=> {
+            console.log(err)
+        })
+
+    },[])
+
 
   return (
     <>
@@ -59,11 +113,14 @@ export const Expensetracker = () => {
                 <div className='submitbtn'>
                 <button type='submit'>ADD</button>
                 </div>
+                {/* <div>
+                    <p>{!error && {error}}</p>
+                </div> */}
                 
             </form>
         </div>
         <div>
-        {
+        {/* {
         data.map((d)=> {
             return (
                 <Expensetrackeritem 
@@ -73,10 +130,25 @@ export const Expensetracker = () => {
                 />
             )
         })
-        
+        } */}
 
 
-        }
+         {
+        dataapi.map((d)=> {
+            return (
+                <Expensetrackeritem 
+                    spend={d.spend}
+                    desc={d.desc} 
+                    category={d.category}
+                />
+            )
+        })
+        }  
+
+
+
+
+
         </div>
 
     </>
